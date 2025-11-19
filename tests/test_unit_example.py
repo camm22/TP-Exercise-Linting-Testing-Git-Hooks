816f5ceb49ca0215f56c66c85131d4bc91554910
Camille <cambordes001@gmail.com>
2025-11-19 11:37:55 +0100

    finish test on hook (all good)

--- tests/test_unit_example.py
+++ tests/test_unit_example.py
@@ -2,8 +2,7 @@ from app.app import capitalize_item
 
 
 def test_capitalize_item():
-    # assert capitalize_item(" hello") == "Hello"
-    # assert capitalize_item("world ") == "World"
-    # assert capitalize_item("  PYTHON ") == "Python"
-    assert capitalize_item("hello") == "wrong"
+    assert capitalize_item(" hello") == "Hello"
+    assert capitalize_item("world ") == "World"
+    assert capitalize_item("  PYTHON ") == "Python"
 
